817958a5f6cf0a0cde1f42157af3f1ce5fe15fcf
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-26 19:26:42 +0300

    add and bind Postres to nest app

---
+++ src/ormconfig.ts
@@ -0,0 +1,12 @@
+import { ConnectionOptions } from 'typeorm';
+
+const config: ConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: 'B12js6dy$',
+  database: 'mediumclone',
+};
+
+export default config;
