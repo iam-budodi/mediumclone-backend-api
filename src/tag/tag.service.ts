38fc3284f18ba6c4720dee3034b69498dfd1c1a9
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-27 00:13:19 +0300

    retrieving tags from db and format them

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from '@app/tag/tag.entity';
 
 @Injectable()
 export class TagService {
-  findAll(): string[] {
-    return ['coffee', 'toffie'];
+  constructor(
+    @InjectRepository(TagEntity)
+    private readonly tagRepository: Repository<TagEntity>,
+  ) {}
+
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
   }
 }
