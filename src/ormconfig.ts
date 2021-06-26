02b80ec1b4c6ddba993e72eb189fc801e288a8ac
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-26 21:39:14 +0300

    creates a table entity and connected to the db

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -7,6 +7,8 @@ const config: ConnectionOptions = {
   username: 'mediumclone',
   password: 'B12js6dy$',
   database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts, .js}'],
+  synchronize: true,
 };
 
 export default config;
