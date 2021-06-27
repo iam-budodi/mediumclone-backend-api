dd974a66c7dd630425b525d0e7ee89ccad1479e1
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-28 01:08:34 +0300

    create dto for user request payload

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly useService: UserService) {}
 
   @Post('users')
-  async createUser(): Promise<any> {
-    return this.useService.createUser();
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<CreateUserDto> {
+    return this.useService.createUser(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
 
 @Injectable()
 export class UserService {
-  async createUser(): Promise<any> {
-    return 'create user from service';
+  async createUser(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+    return createUserDto;
   }
 }
