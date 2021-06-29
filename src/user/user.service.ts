ad0634ad1f8ab66eabd6522a693626cb1a267dce
Japhet Sebastian <japhet.sebastian01@gmail.com>
2021-06-30 00:18:33 +0300

    login authenticated user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from '@app/user/dto/createUser.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { compare } from 'bcrypt';
 import { sign } from 'jsonwebtoken';
 import { UserEntity } from '@app/user/user.entity';
 import { JWT_TOKEN } from '@app/config';
-import { UserResponseInterface } from './types/userResponse.interface';
+import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+import { LoginUserDto } from '@app/user/dto/loginUser.dto';
 
 @Injectable()
 export class UserService {
@@ -15,9 +17,44 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByUserName = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+
+    if (userByEmail || userByUserName) {
+      throw new HttpException(
+        'username or email is taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return this.userRepository.save(newUser);
+  }
+
+  async userLogin(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: loginUserDto.email,
+    });
+
+    if (!userByEmail) {
+      throw new HttpException(
+        'Incorrect email, please try again!',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const match = await compare(loginUserDto.password, userByEmail.password);
+    if (match) return userByEmail;
+    else
+      throw new HttpException(
+        'Incorrect password, please try again!',
+        HttpStatus.UNAUTHORIZED,
+      );
   }
 
   generateJWT(user: UserEntity): string {
